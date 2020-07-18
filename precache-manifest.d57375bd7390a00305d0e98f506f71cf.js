9dbb99b567a3a5bdd9ffde5feedda83e4a5b72ca
Hanna Han <hsh803@gmail.com>
2020-07-18 17:56:02 +0200

    Updates

---
+++ precache-manifest.d57375bd7390a00305d0e98f506f71cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d724f8dee6f420c777ab4ac72f35eb1",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "1da65eda6a0be21780f2",
+    "url": "/portfolio-react/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "ffcd8a0d7800602d89ef",
+    "url": "/portfolio-react/static/css/main.9409cbb2.chunk.css"
+  },
+  {
+    "revision": "1da65eda6a0be21780f2",
+    "url": "/portfolio-react/static/js/2.09aeb73d.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio-react/static/js/2.09aeb73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcd8a0d7800602d89ef",
+    "url": "/portfolio-react/static/js/main.d9f6553d.chunk.js"
+  },
+  {
+    "revision": "edc38ec8943ca1bb9716",
+    "url": "/portfolio-react/static/js/runtime-main.f6a283db.js"
+  }
+]);
