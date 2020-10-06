5e3ea14043da70ea28ffa4af1fb6730ad74472db
Hanna Han <hsh803@gmail.com>
2020-10-06 09:45:32 +0200

    Updates

--- precache-manifest.9b84a5c09ca36bcfd5c60fac6ac4ee74.js
+++ precache-manifest.9b84a5c09ca36bcfd5c60fac6ac4ee74.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79ba0ad7cea24ced37e61fe392f527e8",
+    "revision": "b34c53f79333f43a7fafdf6073e273d4",
     "url": "/portfolio-react/index.html"
   },
   {
-    "revision": "98aaf41df2d04968e53e",
+    "revision": "366029522329a4937cf6",
     "url": "/portfolio-react/static/css/2.e4346227.chunk.css"
   },
   {
-    "revision": "09eefe6f8e70cc633831",
+    "revision": "c99d179237875a6ae2f9",
     "url": "/portfolio-react/static/css/main.ebf5edf6.chunk.css"
   },
   {
-    "revision": "98aaf41df2d04968e53e",
-    "url": "/portfolio-react/static/js/2.38e53a30.chunk.js"
+    "revision": "366029522329a4937cf6",
+    "url": "/portfolio-react/static/js/2.1b8ce9b1.chunk.js"
   },
   {
     "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
-    "url": "/portfolio-react/static/js/2.38e53a30.chunk.js.LICENSE.txt"
+    "url": "/portfolio-react/static/js/2.1b8ce9b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "09eefe6f8e70cc633831",
-    "url": "/portfolio-react/static/js/main.f668210e.chunk.js"
+    "revision": "c99d179237875a6ae2f9",
+    "url": "/portfolio-react/static/js/main.79de9f36.chunk.js"
   },
   {
     "revision": "edc38ec8943ca1bb9716",
